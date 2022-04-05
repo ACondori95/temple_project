d1d35f43afb991198aebe96ea330bb9b5acda01d
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-04-05 12:26:34 -0300

    fieldset

--- js/temples.js
+++ js/temples.js
@@ -7,6 +7,11 @@ function displayTemple(temple) {
   let likeImg = './images/dislike.svg';
   let img1 = document.createElement('img');
   let img2 = document.createElement('img');
+  let legend1 = document.createElement('legend');
+  let fieldset1 = document.createElement('fieldset');
+  let p1 = document.createElement('p');
+  let p2 = document.createElement('p');
+  let a = document.createElement('a');
 
   h3.textContent = `${temple.name}`;
   h3.setAttribute('class', 'temple-name small-shown');
@@ -22,10 +27,19 @@ function displayTemple(temple) {
   img1.setAttribute('loading', 'lazy');
   card.appendChild(img1);
 
+  legend1.textContent = 'Information';
+  fieldset1.appendChild(legend1);
+  p1.textContent = `${temple.address}`;
+  fieldset1.appendChild(p1);
+  p2.textContent = `${temple.telephone}`;
+  fieldset1.appendChild(p2);
+  a.textContent = 'Visit Website';
+  a.setAttribute('href', `${temple.ordinance}`);
+  a.setAttribute('target', '_blank');
+  fieldset1.appendChild(a);
+  card.appendChild(fieldset1);
 
   document.querySelector('#templelist').appendChild(card);
-  // let fieldset1 = document.createElement('fieldset');
-  // let legend1 = document.createElement('legend');
   // let fieldset2 = document.createElement('fieldset');
   // let legend2 = document.createElement('legend');
   // let fieldset3 = document.createElement('fieldset');
@@ -34,27 +48,12 @@ function displayTemple(temple) {
   // let legend4 = document.createElement('legend');
   // let fieldset5 = document.createElement('fieldset');
   // let legend5 = document.createElement('legend');
-  // let p1 = document.createElement('p');
   // let p2 = document.createElement('p');
   // let p3 = document.createElement('p');
   // let p4 = document.createElement('p');
   // let p5 = document.createElement('p');
   // let p6 = document.createElement('p');
-  // let a = document.createElement('a');
 
-  // img1.setAttribute8('alt', `${temple.name} photo`);
-  // img1.setAttribute('loaging', 'lazy');
-  // card.appendChild(img1);
-
-  // legend1.textContent = 'Information';
-  // fieldset1.appendChild(legend1);
-  // p1.textContent = `${temple.address}`;
-  // fieldset1.appendChild(p1);
-  // p2.textContent = `${temple.telephone}`;
-  // fieldset1.appendChild(p2);
-  // a.href = `${temple.ordinance}`;
-  // a.target = '_blank';
-  // a.textContent = 'Visit Website';
   // fieldset1.appendChild(a);
   // card.appendChild(fieldset1);
 
